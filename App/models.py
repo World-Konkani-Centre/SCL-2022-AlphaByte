f6ac34c22be286a61893d6edbcca26381ecee467
unknown <nayaksatvik02@gmail.com>
2022-06-27 09:10:13 +0530

    Responsiveness updated and profile model created

--- App/models.py
+++ App/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Profile(models.Model):
+    user = models.OneToOneField(User,on_delete=models.CASCADE,null=True)
+    name = models.CharField(max_length=50,default='satviksnayak')
+    about = models.TextField(blank=True)
+    location = models.TextField(blank=True)
+    phone = models.IntegerField(blank=True)
+    def __str__(self):
+        return self.name
