2377d936ba32cfb207fb4d6b4aeef1aac60ca628
Satvik S Nayak <nayaksatvik02@gmail.com>
2022-07-02 22:55:43 +0530

    Satvik backend (#17)
    * Combining Branches (#7)
    
    * Update README.md
    
    * added color palette
    
    * Schedule Page UI Started
    
    * Table Design Completed
    
    * Employee Table UI/UX Completed
    
    Co-authored-by: ShravyaMallya <mallyashravya@gmail.com>
    Co-authored-by: Shravya S Mallya <71098888+ShravyaMallya@users.noreply.github.com>
    Co-authored-by: vidyesh-kumar <kumarvidyesh@gmail.com>
    Co-authored-by: Vidyesh Kumar <86671120+vidyesh-kumar@users.noreply.github.com>
    
    * Added .gitignore requirements and updated README.md
    
    * updated base.html,views added login,register pages
    
    * logo and navbar update
    
    * Responsiveness updated and profile model created
    
    Co-authored-by: ShravyaMallya <mallyashravya@gmail.com>
    Co-authored-by: Shravya S Mallya <71098888+ShravyaMallya@users.noreply.github.com>
    Co-authored-by: vidyesh-kumar <kumarvidyesh@gmail.com>
    Co-authored-by: Vidyesh Kumar <86671120+vidyesh-kumar@users.noreply.github.com>

--- App/views.py
+++ App/views.py
@@ -2,62 +2,83 @@ from django.shortcuts import render,redirect
 from django.contrib.auth import authenticate,login,logout
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import Group
 
 
 from .forms import CreateUserForm
+from .decorators import allowed_user, unauthenticated_user
 
 
+def grouplist(user):
+    groups = []
+    if user.groups.exists():
+        for i in range(len(user.groups.all())):
+            groups.append(user.groups.all()[i].name)
+    return groups
+
+@unauthenticated_user
 def register(request):
-    if request.user.is_authenticated:
-        return redirect('home')
-    else:
-        form  = CreateUserForm()
-        if request.method == 'POST':
-            form = CreateUserForm(request.POST)
-            if form.is_valid():
-                form.save()
-                user = form.cleaned_data.get('username')
-                messages.success(request,'Account was created for '+ user)
-                return redirect('login')
-        context = {'form':form}
-        return render(request,'App/register.html',context)
+    form  = CreateUserForm()
+    if request.method == 'POST':
+        form = CreateUserForm(request.POST)
+        if form.is_valid():
+            user=form.save()
+            username = form.cleaned_data.get('username')
+            user.groups.add(request.POST.get('group'))
+            messages.success(request,'Account was created for '+ username)
+            return redirect('login')
+    context = {'form':form}
+    return render(request,'App/register.html',context)
 
+@unauthenticated_user
 def loginPage(request):
-    if request.user.is_authenticated:
-        return redirect('home')
-    else:
-        if request.method == 'POST':
-            username = request.POST.get('username')
-            password = request.POST.get('password')
-            user = authenticate(request,username=username,password=password)
-            if user is not None:
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(request,username=username,password=password)
+        if user is not None:
                 login(request,user)
                 return redirect('home')
-        context = {}
-        return render(request,'App/login.html',context)
+    context = {}
+    return render(request,'App/login.html',context)
 
 def logOutUser(request):
     logout(request)
     return redirect('login')
 
 def home(request):
-    return render(request,'App/home.html')
+    groups = grouplist(request.user)
+    context = {'groups':groups}
+    return render(request,'App/home.html',context)
 
 def Awareness(request):
-    return render(request,'App/home.html')
+    groups = grouplist(request.user)
+    context = {'groups':groups}
+    return render(request,'App/home.html',context)
 
 @login_required(login_url='login')
 def ProfilePage(request):
-    return render(request,'App/profile.html')
+    groups = grouplist(request.user)
+    context = {'groups':groups}
+    return render(request,'App/profile.html',context)
 
 @login_required(login_url='login')
+@allowed_user(allowed_roles=['admin','Employee'])
 def report(request):
-    return render(request,'App/employee.html')
+    groups = grouplist(request.user)
+    context = {'groups':groups}
+    return render(request,'App/employee.html',context)
 
 @login_required(login_url='login')
+@allowed_user(allowed_roles=['admin','Recycler'])
 def schedule(request):
-    return render(request,'App/recyclecomp.html')
+    groups = grouplist(request.user)
+    context = {'groups':groups}
+    return render(request,'App/recyclecomp.html',context)
 
 @login_required(login_url='login')
+@allowed_user(allowed_roles=['admin','Company'])
 def addWaste(request):
-    return render(request,'App/wasteprod.html')
+    groups = grouplist(request.user)
+    context = {'groups':groups}
+    return render(request,'App/wasteprod.html',context)
