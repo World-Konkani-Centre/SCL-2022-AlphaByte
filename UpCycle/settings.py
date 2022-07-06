0f3451250edacf09e5c014b9caa0703b93da72ac
Satvik S Nayak <nayaksatvik02@gmail.com>
2022-07-06 20:06:06 +0530

    Backend Updates (#18)
    * Combining Branches (#7)
    
    * Update README.md
    
    * added color palette
    
    * Schedule Page UI Started
    
    * Table Design Completed
    
    * Employee Table UI/UX Completed
    
    Co-authored-by: ShravyaMallya <mallyashravya@gmail.com>
    Co-authored-by: Shravya S Mallya <71098888+ShravyaMallya@users.noreply.github.com>
    Co-authored-by: vidyesh-kumar <kumarvidyesh@gmail.com>
    Co-authored-by: Vidyesh Kumar <86671120+vidyesh-kumar@users.noreply.github.com>
    
    * Added .gitignore requirements and updated README.md
    
    * updated base.html,views added login,register pages
    
    * logo and navbar update
    
    * Responsiveness updated and profile model created
    
    * Profile Page Updates
    
    * Waste model created and addWaste form. updation of profile update form
    
    * Resolved merge conflicts
    
    Co-authored-by: ShravyaMallya <mallyashravya@gmail.com>
    Co-authored-by: Shravya S Mallya <71098888+ShravyaMallya@users.noreply.github.com>
    Co-authored-by: vidyesh-kumar <kumarvidyesh@gmail.com>
    Co-authored-by: Vidyesh Kumar <86671120+vidyesh-kumar@users.noreply.github.com>

--- UpCycle/settings.py
+++ UpCycle/settings.py
@@ -38,12 +38,17 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    
     'App.apps.AppConfig',
+
+    'phonenumber_field',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    
     'whitenoise.middleware.WhiteNoiseMiddleware',
+    
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
