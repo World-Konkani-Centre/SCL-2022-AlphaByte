c0d19b17f7e1cabff5d9159770ad3004186ca76b
Sahana <sahanaprabhu224@gmail.com>
2022-08-20 19:31:26 +0530

    Profile pages

--- UpCycle/settings.py
+++ UpCycle/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-7d70w(lbvriy(gz0_czwz#!(q$ij)argn@e+w72l!(c!kf&&vp'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
