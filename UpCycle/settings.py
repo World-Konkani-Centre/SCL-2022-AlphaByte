64844daecea7a010f8b294ae49f614f45fc2a309
Sahana <74098334+Sahanaprabhu14@users.noreply.github.com>
2022-08-20 19:34:29 +0530

    Profile Pages (#43)

--- UpCycle/settings.py
+++ UpCycle/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-7d70w(lbvriy(gz0_czwz#!(q$ij)argn@e+w72l!(c!kf&&vp
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
